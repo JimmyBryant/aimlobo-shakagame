ef26253dd565af6014615146828bad1b69bedb5c
shakaflee <62050863+shakaflee@users.noreply.github.com>
2024-08-29 19:33:44 +0800

    Update wooden_fish.js
    1. 把点击的声音固定位mp3格式

--- assets/js/game/wooden_fish/wooden_fish.js
+++ assets/js/game/wooden_fish/wooden_fish.js
@@ -44,8 +44,8 @@ const autoCheckbox = document.querySelector('input[name="auto"]');
 const bgmCheckbox = document.querySelector('input[name="bgm"]');
 
 
-// const bgmClick = new Howl({ src: ["/audio/game/wooden_fish/sound.mp3"] });
-const bgmClick = newSound('/audio/game/wooden_fish/sound', false, 1);
+const bgmClick = new Howl({ src: ["/audio/game/wooden_fish/sound.mp3"] });
+// const bgmClick = newSound('/audio/game/wooden_fish/sound', false, 1);
 const bgmMusic = newSound('/audio/game/wooden_fish/1721013450', true, 0.3);
 
 // 函数
