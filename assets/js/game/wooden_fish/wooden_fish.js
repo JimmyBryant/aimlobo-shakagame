53a0b8ebcb9f37b4914c74f5ffc67a306fd5b967
shakaflee <62050863+shakaflee@users.noreply.github.com>
2024-08-30 16:41:43 +0800

    Update wooden_fish.js
    1. 调整点击声音所在的函数位置：
    -- 从counter()中移除；
    -- 添加进点击事件当中；
    
    -- ios手机端一直不能正常工作，调整进行测试；

--- assets/js/game/wooden_fish/wooden_fish.js
+++ assets/js/game/wooden_fish/wooden_fish.js
@@ -44,8 +44,8 @@ const autoCheckbox = document.querySelector('input[name="auto"]');
 const bgmCheckbox = document.querySelector('input[name="bgm"]');
 
 
-const bgmClick = new Howl({ src: ["/audio/game/wooden_fish/sound.mp3"] });
-// const bgmClick = newSound('/audio/game/wooden_fish/sound', false, 1);
+// const bgmClick = new Howl({ src: ["/audio/game/wooden_fish/sound.mp3"] });
+const bgmClick = newSound('/audio/game/wooden_fish/sound', false, 1);
 const bgmMusic = newSound('/audio/game/wooden_fish/1721013450', true, 0.3);
 
 // 函数
@@ -164,7 +164,7 @@ function counter(message) {
 
     clickCount++;
 
-    bgmClick.play();
+    // bgmClick.play();
 
     writeClickCountToLocal(clickCount);
 
@@ -327,6 +327,8 @@ autoCheckbox.addEventListener("change", () => {
 
             counter(returnUserPrayer());
 
+            bgmClick.play();
+
             setTimeout(initAnimate, setIntervalAnimation * 1000);
 
         }, setIntervalAutoClick * 1000);
@@ -339,20 +341,37 @@ autoCheckbox.addEventListener("change", () => {
     }
 });
 
+// bgmCheckbox.addEventListener("change", () => {
+
+//     if (bgmCheckbox.checked) {
+
+//         if (bgmMusic.state() !== "loaded") {
+
+//             bgmMusic.load();
+//         }
+
+//         bgmMusic.play();
+
+//     } else {
+
+//         bgmMusic.pause();
+//     }
+// });
+
 bgmCheckbox.addEventListener("change", () => {
 
     if (bgmCheckbox.checked) {
 
-        if (bgmMusic.state() !== "loaded") {
+        // if (bgmMusic.state() !== "loaded") {
 
-            bgmMusic.load();
-        }
+        //     bgmMusic.load();
+        // }
 
         bgmMusic.play();
 
     } else {
 
-        bgmMusic.pause();
+        bgmMusic.stop();
     }
 });
 
@@ -365,6 +384,7 @@ woodenFishElement.addEventListener("mousedown", () => {
 
 woodenFishElement.addEventListener("click", () => {
     counter(returnUserPrayer());
+    bgmClick.play();
     setTimeout(initAnimate, setIntervalAnimation * 1000);
 });
 
